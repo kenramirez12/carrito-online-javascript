dcf5e42a1e76c41e77579a48dd361cc324b5c352
Ken Ramirez <kenramirezp@gmail.com>
2022-06-09 22:56:03 -0500

    implementación de estilos

--- main.js
+++ main.js
@@ -1,57 +1,77 @@
 const PRODUCTOS = [
-  {
-    nombre: "Producto #1",
-    precio: "10.00",
-    imagen: "/imagenes/producto.png"
-  },
-  {
-    nombre: "Producto #2",
-    precio: "4.50",
-    imagen: "/imagenes/producto.png"
-  },
-  {
-    nombre: "Producto #3",
-    precio: "43.00",
-    imagen: "/imagenes/producto.png"
-  }
+	{
+		nombre: "Producto #1",
+		precio: "10.00",
+		imagen: "/imagenes/producto.png",
+	},
+	{
+		nombre: "Producto #2",
+		precio: "4.50",
+		imagen: "/imagenes/producto.png",
+	},
+	{
+		nombre: "Producto #2",
+		precio: "4.50",
+		imagen: "/imagenes/producto.png",
+	},
+	{
+		nombre: "Producto #3",
+		precio: "43.00",
+		imagen: "/imagenes/producto.png",
+	},
+	{
+		nombre: "Producto #3",
+		precio: "43.00",
+		imagen: "/imagenes/producto.png",
+	},
 ];
 
 const crearTarjetaProducto = (producto) => {
-  const li = document.createElement("li");
-
-  const imagen = document.createElement("img");
-  imagen.src = producto.imagen;
-
   const contenedor = document.createElement("div");
-
-  const titulo = document.createElement("h3");
-  titulo.innerText = producto.nombre;
-
-  const precio = document.createElement("span");
-  precio.innerText = formatearPrecio(producto.precio);
-
-  const boton = document.createElement("button");
-  boton.innerText = "Agregar al carrito";
-
-  contenedor.append(titulo);
-  contenedor.append(precio);
-  contenedor.append(boton);
+	contenedor.classList.add("col-3");
   
-  li.append(imagen);
-  li.append(contenedor);
+  const card = document.createElement("div");
+  card.classList.add("card");
+  
+	const imagen = document.createElement("img");
+	imagen.classList.add("card-img-top");
+	imagen.src = producto.imagen;
 
-  return li;
-}
+	const contenido = document.createElement("div");
+	contenido.classList.add("card-body");
+
+	const titulo = document.createElement("h5");
+  titulo.classList.add("card-title");
+	titulo.innerText = producto.nombre;
+
+	const precio = document.createElement("span");
+	precio.innerText = formatearPrecio(producto.precio);
+
+	const boton = document.createElement("button");
+	boton.classList.add("d-block", "btn", "btn-outline-primary", "mt-3");
+	boton.innerText = "Agregar al carrito";
+
+	contenido.append(titulo);
+	contenido.append(precio);
+	contenido.append(boton);
+
+  card.append(imagen);
+  card.append(contenido);
+
+  contenedor.append(card);
+
+	return contenedor;
+};
 
 const formatearPrecio = (monto) => {
-  return "S/." + monto;
-}
+	return "S/." + monto;
+};
 
 const init = () => {
-  PRODUCTOS.forEach((producto) => {
-    const tarjetaProducto = crearTarjetaProducto(producto);
-    document.getElementById("lista-productos").append(tarjetaProducto);
-  });
-}
+	PRODUCTOS.forEach((producto) => {
+		const tarjetaProducto = crearTarjetaProducto(producto);
+		document.getElementById("lista-productos").append(tarjetaProducto);
+	});
+};
 
 init();
